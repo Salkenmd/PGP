5679d17e0829fa6b4da4c4ec308653254a6ca957
Salkenmd <151944881+Salkenmd@users.noreply.github.com>
2024-11-24 11:38:42 +0500

    Update Salken 12N Pyplat.py

--- Salken 12N Pyplat.py
+++ Salken 12N Pyplat.py
@@ -3,6 +3,8 @@ import pygame, sys, os
 WIDTH, HEIGHT, GRAVITY, JUMP_STRENGTH, FPS = 800, 600, 0.5, 10, 60
 PLAYER_SIZE, OBSTACLE_SIZE, BORDER_THICKNESS = (32, 32), (80, 10), 10
 DECELERATION, ACCELERATION, MAX_VELOCITY = 0.5, 1, 5
+
+pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
@@ -18,78 +20,113 @@ def load_animation_frames(folder_name):
 class Player:
     def __init__(self):
         self.rect = pygame.Rect(100, HEIGHT - PLAYER_SIZE[1], *PLAYER_SIZE)
-        self.velocity_y, self.velocity_x, self.acceleration_x, self.on_ground, self.jumping = 0, 0, 0, False, False
+        self.velocity_y = 0
+        self.velocity_x = 0
+        self.acceleration_x = 0
+        self.on_ground = False
+        self.jumping = False
+        self.double_jumped = False
         self.animations = {
             "idle": load_animation_frames("Idle"),
             "run": load_animation_frames("Run"),
             "jump": load_animation_frames("Jump"),
-            "falled": load_animation_frames("Landing"),
             "falling_down": load_animation_frames("Downfall"),
             "falling_up": load_animation_frames("Upfall"),
-            "jump_peak": load_animation_frames("Apex")
+            "jump_peak": load_animation_frames("Apex"),
         }
-        self.current_animation, self.current_frame, self.frame_counter, self.frames_per_animation = "idle", 0, 0, 10
+        self.current_animation = "idle"
+        self.current_frame = 0
+        self.frame_counter = 0
+        self.frames_per_animation = 10
+        self.space_held = False
 
     def update(self, keys, obstacles, ground):
         self.acceleration_x = 0
-        if keys[pygame.K_LEFT]: self.acceleration_x = -ACCELERATION
-        elif keys[pygame.K_RIGHT]: self.acceleration_x = ACCELERATION
-        else: self.acceleration_x = -DECELERATION if self.velocity_x > 0 else DECELERATION if self.velocity_x < 0 else 0
-
+        if keys[pygame.K_LEFT]:
+            self.acceleration_x = -ACCELERATION
+        elif keys[pygame.K_RIGHT]:
+            self.acceleration_x = ACCELERATION
+        else:
+            self.acceleration_x = -DECELERATION if self.velocity_x > 0 else DECELERATION if self.velocity_x < 0 else 0
         self.velocity_x += self.acceleration_x
         self.velocity_x = max(min(self.velocity_x, MAX_VELOCITY), -MAX_VELOCITY)
         self.rect.x += self.velocity_x
 
-        if keys[pygame.K_SPACE] and self.on_ground and not self.jumping:
-            self.velocity_y = -JUMP_STRENGTH
-            self.jumping, self.on_ground = True, False
+        if keys[pygame.K_SPACE] and not self.space_held:
+            if self.on_ground:
+                self.velocity_y = -JUMP_STRENGTH
+                self.jumping = True
+                self.on_ground = False
+                self.double_jumped = False
+            elif not self.on_ground and not self.double_jumped:
+                self.velocity_y = -JUMP_STRENGTH
+                self.jumping = True
+                self.double_jumped = True
+            self.space_held = True
+        elif not keys[pygame.K_SPACE]:
+            self.space_held = False
 
-        if not self.on_ground: self.velocity_y += GRAVITY
+        if not self.on_ground:
+            self.velocity_y += GRAVITY
         self.rect.y += self.velocity_y
         self.check_collisions(obstacles, ground)
-
-        if self.jumping:
-            if self.velocity_y > 0: self.set_animation("falling_down")
-            elif self.velocity_y == 0: self.set_animation("jump_peak")
-            else: self.set_animation("falling_up")
-        elif self.on_ground:
-            if self.velocity_x == 0: self.set_animation("idle")
-            else: self.set_animation("run")
-
+        self.update_animation()
         self.frame_counter += 1
         if self.frame_counter >= self.frames_per_animation:
             self.current_frame = (self.current_frame + 1) % len(self.animations[self.current_animation])
             self.frame_counter = 0
 
+    def update_animation(self):
+        if self.jumping:
+            if self.velocity_y > 0:
+                self.set_animation("falling_down")
+            elif self.velocity_y == 0:
+                self.set_animation("jump_peak")
+            else:
+                self.set_animation("falling_up")
+        elif self.on_ground:
+            if self.velocity_x == 0:
+                self.set_animation("idle")
+            else:
+                self.set_animation("run")
+
     def set_animation(self, animation_name):
         if self.current_animation != animation_name:
             self.current_animation = animation_name
             self.current_frame = 0
+            self.frame_counter = 0
 
     def check_collisions(self, obstacles, ground):
         self.on_ground = False
         for obs in obstacles:
             if self.rect.colliderect(obs.rect):
-                if self.velocity_y > 0: 
+                if self.velocity_y > 0:
                     self.rect.bottom = obs.rect.top
-                    self.velocity_y, self.on_ground = 0, True
-                elif self.velocity_y < 0: 
+                    self.velocity_y = 0
+                    self.on_ground = True
+                    self.jumping = False
+                elif self.velocity_y < 0:
                     self.rect.top = obs.rect.bottom
                     self.velocity_y = 0
-
         if self.rect.colliderect(ground.rect):
             self.rect.bottom = ground.rect.top
-            self.velocity_y, self.on_ground, self.jumping = 0, True, False
+            self.velocity_y = 0
+            self.on_ground = True
+            self.jumping = False
         elif self.velocity_y > 0:
             self.on_ground = False
 
     def draw(self, surface):
         frames = self.animations[self.current_animation]
-        surface.blit(pygame.transform.flip(frames[self.current_frame], True, False) if self.velocity_x < 0 else frames[self.current_frame], self.rect.topleft)
+        offset_x, offset_y = (self.rect.width - frames[self.current_frame].get_width()) // 2, (self.rect.height - frames[self.current_frame].get_height()) // 2
+        surface.blit(
+            pygame.transform.flip(frames[self.current_frame], True, False) if self.velocity_x < 0 else frames[self.current_frame],
+            (self.rect.x + offset_x, self.rect.y + offset_y)
+        )
 
 class Obstacle:
     def __init__(self, x, y):
-        self.rect = pygame.Rect(x + 10, y + 5, *OBSTACLE_SIZE)
+        self.rect = pygame.Rect(x, y, *OBSTACLE_SIZE)
 
 class Ground:
     def __init__(self, x, y, width, height):
